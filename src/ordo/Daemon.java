4e4a63fa2e32a8c2071305eb24c8ed126e9aa581
Zor0d <ddiochot@gmail.com>
2017-12-07 16:07:02 +0100

    modifications du demon

--- src/ordo/Daemon.java
+++ src/ordo/Daemon.java
@@ -1,14 +1,22 @@
 package ordo;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
 
 import map.Mapper;
+import map.Reducer;
 import formats.Format;
+import formats.Format.OpenMode;
+import formats.KV;
 
 public class Daemon extends UnicastRemoteObject implements IDaemon {
 
@@ -20,9 +28,11 @@ public class Daemon extends UnicastRemoteObject implements IDaemon {
 	public static int rmiPort = 5000;
 	public static int hbPort = 5002;
 	
-	public String localHost = null;
-	public int localPort = 0;
-	public HeartBeatEmitter hb;
+	private String localHost = null;
+	private int localPort = 0;
+	private HeartBeatEmitter hb;
+	private HashMap<String, Integer> results;
+	
 	
 	public Daemon(String localHost, int port) throws UnknownHostException, IOException {
 		super();
@@ -33,7 +43,20 @@ public class Daemon extends UnicastRemoteObject implements IDaemon {
 		
 	}
 	
-	public void runMap (Mapper m, Format reader, Format writer, CallBack cb) throws RemoteException {
+	public void runMap(Mapper m, Format reader, Format writer, CallBack cb) throws RemoteException {
+		/*
+		 * On crée un thread esclave qui va exécuter le map
+		 */
+		MapSlave s = new MapSlave(reader, writer, m, results, cb, this.localHost, this.localPort);
+		s.start();
+	}
+	
+	public void runReduce (Reducer r, Format reader, Format writer, CallBack cb) throws RemoteException {
+		/*
+		 * On crée un thread esclave qui va exécuter le reduce
+		 */
+		ReduceSlave s = new ReduceSlave(reader, writer, r, results, cb, this.localHost, this.localPort);
+		s.start();
 	}
 	
 	public String getHostname() throws RemoteException {
@@ -51,3 +74,69 @@ public class Daemon extends UnicastRemoteObject implements IDaemon {
 		}
 	}
 }
+
+class MapSlave extends Thread {
+
+	private ServerSocket ss;
+	private Socket s;
+	private Format reader;
+	private Format writer;
+	private HashMap<String, Integer> results;
+	private CallBack cb;
+	private Mapper m;
+	private String localHost;
+	private int localPort;
+
+
+	public MapSlave(Format reader, Format writer, Mapper m, 
+			HashMap<String, Integer> results, CallBack cb, String localHost, int localPort) {
+		this.reader = reader;
+		/* On ouvre les fichiers utiles au map */
+		this.reader= reader;
+		this.writer=writer;
+		this.cb = cb;
+		this.results = results;
+		this.m = m;
+	}
+
+	public void run() {
+
+		this.reader.open(OpenMode.R);
+		this.writer.open(OpenMode.W);
+		this.m.map(reader, writer);
+
+		this.cb.isTerminated(this.localHost, this.localPort);
+	}
+}
+
+class ReduceSlave extends Thread {
+
+	private ServerSocket ss;
+	private Socket s;
+	private Format reader;
+	private Format writer;
+	private HashMap<String, Integer> results;
+	private CallBack cb;
+	private Reducer r;
+	private String localHost;
+	private int localPort;
+
+
+	public ReduceSlave(Format reader, Format writer, Reducer r, 
+			HashMap<String, Integer> results, CallBack cb, String localHost, int localPort) {
+		this.reader = reader;
+		/* On ouvre les fichiers utiles au map */
+		this.reader= reader;
+		this.writer=writer;
+		this.cb = cb;
+		this.results = results;
+		this.r = r;
+	}
+
+	public void run() {
+		// TODO
+
+		this.cb.isTerminated(this.localHost, this.localPort);
+	}
+}
+
